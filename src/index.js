cce4270a568522b43eb097a95bd5d2888c26d771
hernan verar <hernanverar@gmail.com>
2023-08-30 15:19:07 -0700

    This commit add a good build had to change our values and changed parameters in our functions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,66 @@
 import './css/styles.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {deathFlower} from './plant.js';
-
-const getValue = (collection, object, id, callback) => {
-  if (collection.lenght === 0) {
-    return;
-  }
-const [currentElement, ...remainigElements] = collection;
-callback(currentElement, object, id);
-
-getValues(remainigElements, object, id, callback);
-}
+import { deathFlowerControl, blackBerryControl } from './plant.js';
 
 const showValues = (key, object, id) => {
   const p = document.createElement("p");
   p.innerText = `${key}: ${object[key]}`;
   document.getElementById(`${id}`).append(p);
-}
+};
 
-//UI logic
-
+// UI logic
 window.onload = () => {
-  document.getElementById("show-hm").onclick = () => {
+  document.getElementById("show-df").onclick = () => {
     const currentState = deathFlowerControl();
-    getValues(Object.keys(currentState), currentState, 'all-values-hm', showValues);
+    Object.keys(currentState).forEach(key => {
+      showValues(key, currentState, 'all-values-df');
+    });
   };
-} 
+
+  document.getElementById("show-bm").onclick = () => {
+    const currentState = blackBerryControl();
+    Object.keys(currentState).forEach(key => {
+      showValues(key, currentState, 'all-values-bm');
+    });
+  };
+};
+
+
+
+// import './css/styles.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import {deathFlowerControl, blackBerryControl} from './plant.js';
+
+// const getValue = (collection, object, id, callback) => {
+//   if (collection.lenght === 0) {
+//     return;
+//   }
+// const [currentElement, ...remainigElements] = collection;
+// callback(currentElement, object, id);
+
+// getValues(remainigElements, object, id, callback);
+// }
+
+// const showValues = (key, object, id) => {
+//   const p = document.createElement("p");
+//   p.innerText = `${key}: ${object[key]}`;
+//   document.getElementById(`${id}`).append(p);
+// }
+
+// //UI logic
+
+// window.onload = () => {
+//   document.getElementById("show-hm").onclick = () => {
+//     const currentState = deathFlowerControl();
+//     getValues(Object.keys(currentState), currentState, 'all-values-df', showValues);
+//   };
+// } 
+
+// window.onload = () => {
+//   document.getElementById("show-hm").onclick = () => {
+//     const currentState = blackBerryControl();
+//     getValues(Object.keys(currentState), currentState, 'all-values-bm', showValues);
+//   };
+// } 
--- src/plant.js
+++ src/plant.js
@@ -1,32 +1,50 @@
 import { changeState, storeState } from './business.js';
 
+// Define the state changes
+const beauty = changeState("bloom")(3);
+const damage = changeState("oil")(5);
+// const danger = changeState("venom")(1);
 
-// const feed = changeState ("leaf")(3);
-const plant = changeState("oil")(5);
 
 
 
-const feedPlant = (name) => {
-const plant = {name: name };
-  return  {
-  ...poison,
-  ...damage(plant),
-  ...beauty(plant)
-  }
+// Define feedPlant and deathFlower functions
+function feedPlant(name) {
+  const poison = { name: name };
+  return {
+    ...poison,
+    ...damage(poison),
+    ...beauty(poison)
+  };
 }
 
 const deathFlower = (name) => {
-  const poison = {name: name };
-    return  {
+  const poison = poison(name);
+  return {
     ...poison,
-    ...damage(plant),
-    ...beauty(plant)
-    };
+    ...damage(poison),
+    ...beauty(poison),
   };
+}
+
+const blackBerry = (name) => {
+  const poison = poison(name);
+  return {
+    ...poison,
+    ...damage(poison),
+    ...beauty(poison),
+  };
+}
+
+const feedPlantState = feedPlant("Feed Plant");
+const deathFlowerState = deathFlower("Death Flower");
+const blackBerryState = blackBerry("Black Berry");
 
 
-  const deathFlower = feedPlant("Death Flower");
+export const feedPlantControl = storeState(feedPlantState);
+export const deathFlowerControl = storeState(deathFlowerState);
+export const blackBerryControl = storeState(blackBerryState);
 
-  export const deathFlowerControl = storeState(dragonFlower);
 
- 
+
+
